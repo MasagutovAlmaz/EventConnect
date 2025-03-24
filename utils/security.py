95469aa13da6680950e1b3fe470b7a5428fb440b
Probnic <almazmasagutov062@gmail.com>
2025-03-24 21:22:21 +0700

    logic hash password and otp

--- utils/security.py
+++ utils/security.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -7,3 +10,9 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def generate_otp(length=6):
+    return ''.join(random.choices(string.digits, k=length))
