7af97196bc3b17173630099068b639c3045c3e66
Probnic <almazmasagutov062@gmail.com>
2025-03-25 14:29:43 +0700

    comment

--- utils/security.py
+++ utils/security.py
@@ -1,38 +1,38 @@
-import os
-import random
-import string
-from datetime import timedelta, datetime
-
-from dotenv import load_dotenv
-from jose import jwt
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(password, hashed_password)
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def generate_otp(length=6):
-    return ''.join(random.choices(string.digits, k=length))
-
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+# import os
+# import random
+# import string
+# from datetime import timedelta, datetime
+#
+# from dotenv import load_dotenv
+# from jose import jwt
+# from passlib.context import CryptContext
+#
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+#
+# load_dotenv()
+#
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# ALGORITHM = os.getenv("ALGORITHM")
+# ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+#
+# def hash_password(password: str) -> str:
+#     return pwd_context.hash(password)
+#
+# def verify_password(password: str, hashed_password: str) -> bool:
+#     return pwd_context.verify(password, hashed_password)
+#
+# def get_password_hash(password: str) -> str:
+#     return pwd_context.hash(password)
+#
+# def generate_otp(length=6):
+#     return ''.join(random.choices(string.digits, k=length))
+#
+# def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
+#     to_encode = data.copy()
+#     expire = datetime.utcnow() + expires_delta
+#     to_encode.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encoded_jwt
 
 
 
