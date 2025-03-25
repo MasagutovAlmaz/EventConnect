f92df1087880e16be64bf2a20ffa2222adc4bc20
Probnic <almazmasagutov062@gmail.com>
2025-03-25 14:34:21 +0700

    doc

--- models/user.py
+++ models/user.py
@@ -2,6 +2,19 @@ from pydantic import BaseModel, EmailStr
 
 
 class RegistrationRequest(BaseModel):
+
+    """
+    Модель запроса на регистрацию пользователя
+    {
+  "full_name": "Иванов Иван Иванович",
+  "email": "ivanov062@gmail.com",
+  "phone": "+79232363096",
+  "telegram": "@ivanov23",
+  "company": "-", тут company по желанию, если есть
+  "agree_personal_data": true, тут мы не пропускаем дальше если не стоит галочка
+  "agree_terms": true, тут мы не пропускаем дальше если не стоит галочка
+}
+    """
     full_name: str
     email: EmailStr
     phone: str
