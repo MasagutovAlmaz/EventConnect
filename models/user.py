e253c0582aec8dd24e78562f3d0793adea3cb814
Probnic <almazmasagutov062@gmail.com>
2025-03-25 14:29:34 +0700

    add telegram and company

--- models/user.py
+++ models/user.py
@@ -1,28 +1,22 @@
 from pydantic import BaseModel, EmailStr
 
 
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
+class RegistrationRequest(BaseModel):
+    full_name: str
+    email: EmailStr
+    phone: str
+    telegram: str
+    company: str | None = None
+    agree_personal_data: bool
+    agree_terms: bool
 
-class UserResponse(BaseModel):
+class RegisterResponse(BaseModel):
     id: int
-    name: str
-    email: str
-    is_verified: bool
+    full_name: str
+    email: EmailStr
+    phone: str
+    telegram: str
+    company: str | None = None
 
     class Config:
         from_attributes = True
-
-class RegisterResponse(BaseModel):
-    message: str
-    user: UserResponse
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class OTPVerify(BaseModel):
-    email: EmailStr
-    otp: str
