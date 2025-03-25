32f6a4ce6e8d321531a416900092344bf27b0128
Probnic <almazmasagutov062@gmail.com>
2025-03-25 14:30:05 +0700

    did the right thing

--- db/models.py
+++ db/models.py
@@ -1,17 +1,24 @@
-from sqlalchemy import BigInteger, Column, String, Boolean, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import BigInteger, Column, String, Boolean
+from sqlalchemy.orm import declarative_base, validates
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
+class Register(Base):
+    __tablename__ = "registrations"
 
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    name = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False, index=True)
-    password_hash = Column(String(100), nullable=False)
-    is_verified = Column(Boolean, default=False)
-    verification_code = Column(String, nullable=True)
-    code_expiration = Column(DateTime, nullable=False)
+    id = Column(BigInteger, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, index=True)
+    phone = Column(String, index=True)
+    telegram = Column(String, index=True)
+    company = Column(String, index=True, nullable=True)
+    agree_personal_data = Column(Boolean, index=True)
+    agree_terms = Column(Boolean, index=True)
+
+    @validates('agree_personal_data', 'agree_terms')
+    def validate_agreements(self, key, value):
+        if not value:
+            raise ValueError("Необходимо согласиться с условиями")
+        return value
 
 
