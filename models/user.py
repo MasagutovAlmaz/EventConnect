8695e73940fd438bcf0758a93d55609a4a246064
Probnic <almazmasagutov062@gmail.com>
2025-03-26 22:50:15 +0700

    create model UserResponse

--- models/user.py
+++ models/user.py
@@ -1,35 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
-class RegistrationRequest(BaseModel):
-
-    """
-    Модель запроса на регистрацию пользователя
-    {
-  "full_name": "Иванов Иван Иванович",
-  "email": "ivanov062@gmail.com",
-  "phone": "+79232363096",
-  "telegram": "@ivanov23",
-  "company": "-", тут company по желанию, если есть
-  "agree_personal_data": true, тут мы не пропускаем дальше если не стоит галочка
-  "agree_terms": true, тут мы не пропускаем дальше если не стоит галочка
-}
-    """
-    full_name: str
-    email: EmailStr
-    phone: str
-    telegram: str
-    company: str | None = None
-    agree_personal_data: bool
-    agree_terms: bool
-
-class RegisterResponse(BaseModel):
+class UserResponse(BaseModel):
     id: int
     full_name: str
-    email: EmailStr
+    email: str
     phone: str
     telegram: str
     company: str | None = None
-
-    class Config:
-        from_attributes = True
+    event_id: int
