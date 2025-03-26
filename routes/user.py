4fffee28113885e97eeb2134c43acac33f7508e4
Probnic <almazmasagutov062@gmail.com>
2025-03-26 22:49:30 +0700

    create func get_user

--- routes/user.py
+++ routes/user.py
@@ -1,86 +1,17 @@
-import logging
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 
 from db.database import get_db
-from db.models import Register
-from models.user import RegistrationRequest, RegisterResponse
+from db.models import RegisterEvent
+from models.user import UserResponse
 
 router = APIRouter()
 
+@router.get("/user", response_model=UserResponse)
+async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
+    user = await db.get(RegisterEvent, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
 
-@router.post("/register", tags=["register"], summary="Регистрация нового пользователя",
-    description="""
-    ## Описание:
-    Регистрирует пользователя в системе.  
-    **Обязательные условия:**  
-    - Пользователь должен согласиться с обработкой данных (`agree_personal_data=True`).  
-    - Пользователь должен принять условия оферты (`agree_terms=True`).  
-
-    ## Пример запроса:
-    ```json
-    {
-      "full_name": "Иванов Иван Иванович",
-      "email": "ivonov32434@gmail.com",
-      "phone": "+79991234567",
-      "telegram": "@ivanov233",
-      "agree_personal_data": true,
-      "agree_terms": true
-    }
-    ```
-    """,
-    responses={
-        status.HTTP_200_OK: {
-            "description": "Успешная регистрация",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "id": 1,
-                        "full_name": "Иванов Иван Иванович",
-                        "email": "ivonov32434@gmail.com",
-                        "phone": "+79991234567",
-                        "telegram": "@ivanov233"
-                    }
-                }
-            }
-        },
-        status.HTTP_400_BAD_REQUEST: {
-            "description": "Ошибка валидации",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Необходимо согласиться с условиями"
-                    }
-                }
-            }
-        }
-    }
-)
-async def register(data: RegistrationRequest, db: AsyncSession = Depends(get_db)):
-    if not data.agree_personal_data or not data.agree_terms:
-        raise HTTPException(status_code=400, detail="Необходимо согласие с условиями")
-
-
-    new_registration = Register(
-        full_name=data.full_name,
-        email=str(data.email),
-        phone=data.phone,
-        telegram=data.telegram,
-        company=data.company,
-        agree_personal_data=data.agree_personal_data,
-        agree_terms=data.agree_terms,
-    )
-    db.add(new_registration)
-    try:
-        await db.commit()
-        await db.refresh(new_registration)
-    except Exception as e:
-        logging.error(f"Ошибка при сохранении пользователя: {e}")
-        await db.rollback()
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-    reg_response = RegisterResponse.from_orm(new_registration)
-
-    return reg_response
+    user_response = UserResponse(**user.__dict__)
+    return user_response
