0dacf700d787da60552f1a14a26ea5fe6c237880
MasagutovAlmaz <almazmasagutov062@gmail.com>
2025-04-03 01:36:14 +0700

    feat: update Event model with timezone support and required fields

--- db/event.py
+++ db/event.py
@@ -1,3 +1,5 @@
+from time import timezone
+
 from sqlalchemy import Column, String, Boolean, Integer, DateTime, select, func, Sequence
 from sqlalchemy.orm import relationship, column_property
 
@@ -9,11 +11,13 @@ class Event(Base):
     __tablename__ = "events"
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
-    date = Column(DateTime(timezone=False))
+    date = Column(DateTime(timezone=True), nullable=False)
+    current_date = Column(DateTime(timezone=True), nullable=False)
+    end_date = Column(DateTime(timezone=True), nullable=False)
     location = Column(String, nullable=False)
     timezone = Column(String, default="UTC+3")
     is_active = Column(Boolean, default=True)
-    image_url = Column(String, nullable=True)
+    image_url = Column(String, nullable=False)
     time = Column(String, nullable=True)
 
     registrations = relationship("RegisterEvent", back_populates="event")
